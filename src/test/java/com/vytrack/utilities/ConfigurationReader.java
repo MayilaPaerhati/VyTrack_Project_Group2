129ef00b069fc20961545cef256278fd48b9f353
Mayira <mahira223@hotmail.com>
2022-10-06 09:54:55 -0400

    ConfigurationReader class added

---
+++ src/test/java/com/vytrack/utilities/ConfigurationReader.java
@@ -0,0 +1,30 @@
+package com.vytrack.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+    private static Properties properties = new Properties();
+
+    static {
+
+        try {
+            // Creates a file object, allows us to connect to a file
+            FileInputStream file = new FileInputStream("config.properties");
+
+            // loads the files into the properties object
+            properties.load(file);
+
+        }catch(IOException e){
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public static String getProperty(String key){
+        return properties.getProperty(key);
+    }
+}
