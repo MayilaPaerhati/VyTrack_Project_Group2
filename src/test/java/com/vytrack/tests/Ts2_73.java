5fde928bb3a525ed962fcc393620d14f3340a0da
Anar Ismayilov <anarissoy@gmail.com>
2022-10-16 03:19:39 -0400

    TS2-73.java update

--- src/test/java/com/vytrack/tests/Ts2_73.java
+++ src/test/java/com/vytrack/tests/Ts2_73.java
@@ -1,5 +1,13 @@
 package com.vytrack.tests;
 
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
 public class Ts2_73 {
 
     /*
@@ -10,4 +18,17 @@ public class Ts2_73 {
                TYPE, TOTAL PRICE, DATE
      */
 
+    @Test
+    public void testThreeColumn() {
+
+        //Driver.getDriver().get("https://qa2.vytrack.com/user/login");
+        //Driver.getDriver().get(ConfigurationReader.getProperty("env"));
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //driver.get("https://qa2.vytrack.com/user/login");
+        driver.get(ConfigurationReader.getProperty("env"));
+
+    }
 }
