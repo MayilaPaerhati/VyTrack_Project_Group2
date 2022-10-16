bf915546484ce9968b65cc33965a4c0ad634cb91
Anar Ismayilov <anarissoy@gmail.com>
2022-10-16 03:05:41 -0400

    TS2-73.java create

---
+++ src/test/java/com/vytrack/tests/Ts2_73.java
@@ -0,0 +1,13 @@
+package com.vytrack.tests;
+
+public class Ts2_73 {
+
+    /*
+    As a user, I want to manage filters on the Vehicle Costs page. (Web-table and checkbox)
+
+    AC #1: Users should see three columns on the Vehicle Cost page.
+               Expected Column names:
+               TYPE, TOTAL PRICE, DATE
+     */
+
+}
