8c49d4c5fcbe4bf96fc62ecf4e3f4e80158efd53
Anar Ismayilov <anarissoy@gmail.com>
2022-10-16 15:54:42 -0400

    TS2-73.java done

--- src/test/java/com/vytrack/tests/Ts2_73.java
+++ src/test/java/com/vytrack/tests/Ts2_73.java
@@ -1,15 +1,19 @@
-package com.vytrack.tests;
+package com.cydeo.tests.myprojects;
 
+import com.vytrack.utilities.*;
+import com.vytrack.utilities.WebDriverFactory;
 import com.vytrack.utilities.BrowserUtils;
 import com.vytrack.utilities.ConfigurationReader;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.hc.core5.reactor.Command;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class Ts2_73 {
 
@@ -21,28 +25,26 @@ public class Ts2_73 {
                TYPE, TOTAL PRICE, DATE
      */
 
+
     WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
-        //driver.get("https://qa2.vytrack.com/user/login");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(ConfigurationReader.getProperty("env"));
+        BrowserUtils.sleep(2);
     }
 
-    @Test
-    public void testThreeColumnAsDriver() {
-
-        //Driver.getDriver().get("https://qa2.vytrack.com/user/login");
-        //Driver.getDriver().get(ConfigurationReader.getProperty("env"));
+    @Test (priority = 1)
+    public void testThreeColumnAsDriver(){
 
         WebElement enterUsername = driver.findElement(By.xpath("//input[@placeholder='Username or Email']"));
-        enterUsername.sendKeys(ConfigurationReader.getProperty("driver1"));
+        enterUsername.sendKeys("user6");
 
-        WebElement enterPassword = driver.findElement(By.xpath("//input[@placeholder='Password']"));
-        enterPassword.sendKeys(ConfigurationReader.getProperty("password"));
+        WebElement password = driver.findElement(By.xpath("//input[@placeholder='Password']"));
+        password.sendKeys("UserUser123");
 
         WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
         loginButton.click();
@@ -64,10 +66,72 @@ public class Ts2_73 {
 
         WebElement dateDisplay = driver.findElement(By.xpath("//span[.='Date']"));
         Assert.assertEquals(dateDisplay.getText(),"DATE", "Displaying DATE FAILED!");
+    }
 
+    @Test (priority = 2)
+    public void testThreeColumnAsStoremanager(){
 
+        WebElement enterUsername = driver.findElement(By.xpath("//input[@placeholder='Username or Email']"));
+        enterUsername.sendKeys("storemanager56");
+
+        WebElement password = driver.findElement(By.xpath("//input[@placeholder='Password']"));
+        password.sendKeys("UserUser123");
+
+        WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginButton.click();
+        BrowserUtils.sleep(2);
+
+        WebElement fleetButton = driver.findElement(By.xpath("(//span[@class='title title-level-1'])[2]"));
+        fleetButton.click();
+        BrowserUtils.sleep(2);
+
+        WebElement vehicleCostsBtn = driver.findElement(By.xpath("//span[.='Vehicle Costs']"));
+        vehicleCostsBtn.click();
+        BrowserUtils.sleep(2);
+
+        WebElement typeDisplay = driver.findElement(By.xpath("//span[.='Type']"));
+        Assert.assertEquals(typeDisplay.getText(), "TYPE", "Displaying TYPE FAILED!");
+
+        WebElement totalPriceDisplay = driver.findElement(By.xpath("//span[.='Total Price']"));
+        Assert.assertEquals(totalPriceDisplay.getText(), "TOTAL PRICE", "Displaying TOTAL PRICE FAILED!");
+
+        WebElement dateDisplay = driver.findElement(By.xpath("//span[.='Date']"));
+        Assert.assertEquals(dateDisplay.getText(),"DATE", "Displaying DATE FAILED!");
+    }
+
+    @Test (priority = 3)
+    public void testThreeColumnAsSalesmanager(){
+
+        WebElement enterUsername = driver.findElement(By.xpath("//input[@placeholder='Username or Email']"));
+        enterUsername.sendKeys("salesmanager106");
+
+        WebElement password = driver.findElement(By.xpath("//input[@placeholder='Password']"));
+        password.sendKeys("UserUser123");
+
+        WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginButton.click();
+        BrowserUtils.sleep(2);
+
+        WebElement fleetButton = driver.findElement(By.xpath("(//span[@class='title title-level-1'])[2]"));
+        fleetButton.click();
+        BrowserUtils.sleep(2);
+
+        WebElement vehicleCostsBtn = driver.findElement(By.xpath("//span[.='Vehicle Costs']"));
+        vehicleCostsBtn.click();
+        BrowserUtils.sleep(2);
+
+        WebElement typeDisplay = driver.findElement(By.xpath("//span[.='Type']"));
+        Assert.assertEquals(typeDisplay.getText(), "TYPE", "Displaying TYPE FAILED!");
+
+        WebElement totalPriceDisplay = driver.findElement(By.xpath("//span[.='Total Price']"));
+        Assert.assertEquals(totalPriceDisplay.getText(), "TOTAL PRICE", "Displaying TOTAL PRICE FAILED!");
+
+        WebElement dateDisplay = driver.findElement(By.xpath("//span[.='Date']"));
+        Assert.assertEquals(dateDisplay.getText(),"DATE", "Displaying DATE FAILED!");
+    }
+
+    @AfterMethod
+    public void tearDown() {
         driver.quit();
-
-
     }
 }
